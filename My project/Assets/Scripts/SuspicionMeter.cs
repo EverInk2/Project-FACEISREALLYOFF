156d7605c2601622aa7dbd7d4d722d6f343f873d
EverInk2 <adropofink2@gmail.com>
2026-01-31 16:10:52 -0500

    added initial suspicion

--- My project/Assets/Scripts/SuspicionMeter.cs
+++ My project/Assets/Scripts/SuspicionMeter.cs
@@ -12,7 +12,7 @@ public class SuspicionMeter : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        suspicion = 0;
     }
 
     // Update is called once per frame
