8823066b376ba1eaea6ddd8ea4cb4d6d94b3a197
EverInk2 <adropofink2@gmail.com>
2026-01-31 16:07:42 -0500

    Children of Suspicion

--- My project/Assets/PathFindingScript.cs
+++ My project/Assets/PathFindingScript.cs
@@ -21,47 +21,47 @@ public class PathFindingScript : MonoBehaviour
         
     }
 
-    private List<int[]> FindPath(Vector2 startPos, Vector2 targetPos)
-    {
-        transform.position = startPos;
-        while (Mathf.Abs(targetPos.x - transform.position.x) < 0.8 && Mathf.Abs(targetPos.y - transform.position.y) < 0.8)
-        {
-            if (Mathf.Abs(targetPos.x - transform.position.x) >= Mathf.Abs(targetPos.y - transform.position.y))
-            {
-                if (targetPos.x > transform.position.x + 0.5)
-                {
-                    directions = new int[] { 1, 0 };
-                    path.Add(directions);
-                }
-                else if (targetPos.x < transform.position.x - 0.5)
-                {
-                    directions = new int[] { -1, 0 };
-                    path.Add(directions);
-                }
-                else
-                {
-                    directions = new int[] { 0, 0 };
-                    return path;
-                }
-            }
-            else
-            {
-                if (targetPos.y > transform.position.y + 0.5)
-                {
-                    directions = new int[] { 0, 1 };
-                    path.Add(directions);
-                }
-                else if (targetPos.y < transform.position.y - 0.5)
-                {
-                    directions = new int[] { 0, -1 };
-                    path.Add(directions);
-                }
-                else
-                {
-                    directions = new int[] { 0, 0 };
-                    return path;
-                }
-            }
-        }
-    }
+    //private List<int[]> FindPath(Vector2 startPos, Vector2 targetPos)
+    //{
+    //    transform.position = startPos;
+    //    while (Mathf.Abs(targetPos.x - transform.position.x) < 0.8 && Mathf.Abs(targetPos.y - transform.position.y) < 0.8)
+    //    {
+    //        if (Mathf.Abs(targetPos.x - transform.position.x) >= Mathf.Abs(targetPos.y - transform.position.y))
+    //        {
+    //            if (targetPos.x > transform.position.x + 0.5)
+    //            {
+    //                directions = new int[] { 1, 0 };
+    //                path.Add(directions);
+    //            }
+    //            else if (targetPos.x < transform.position.x - 0.5)
+    //            {
+    //                directions = new int[] { -1, 0 };
+    //                path.Add(directions);
+    //            }
+    //            else
+    //            {
+    //                directions = new int[] { 0, 0 };
+    //                return path;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            if (targetPos.y > transform.position.y + 0.5)
+    //            {
+    //                directions = new int[] { 0, 1 };
+    //                path.Add(directions);
+    //            }
+    //            else if (targetPos.y < transform.position.y - 0.5)
+    //            {
+    //                directions = new int[] { 0, -1 };
+    //                path.Add(directions);
+    //            }
+    //            else
+    //            {
+    //                directions = new int[] { 0, 0 };
+    //                return path;
+    //            }
+    //        }
+    //    }
+    //}
 }
